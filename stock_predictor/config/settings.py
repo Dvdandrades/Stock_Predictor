083f4ec625770917da270b893a1e3901b77e02dd
Dvdandrades <dvdandrades@gmail.com>
2026-03-09 18:24:45 +0100

    add database connection

--- stock_predictor/config/settings.py
+++ stock_predictor/config/settings.py
@@ -1,6 +1,7 @@
 from pydantic_settings import BaseSettings
 from functools import lru_cache
 
+
 class Settings(BaseSettings):
     secret_key: str
     algorithm: str = "HS256"
@@ -11,6 +12,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+
 
 @lru_cache
 def get_settings():
---
+++ stock_predictor/dependencies/database.py
@@ -0,0 +1,9 @@
+from stock_predictor.database.session import SessionLocal
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
