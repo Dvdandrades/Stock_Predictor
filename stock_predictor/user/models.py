6e325a5bfe5eb0b739b52d4a9b6e54cfbd5c8629
Dvdandrades <dvdandrades@gmail.com>
2026-03-09 19:42:31 +0100

    add user model

---
+++ stock_predictor/user/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, String, Integer
+from stock_predictor.database.session import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
