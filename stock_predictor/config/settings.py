876a25a62bb91fe8adb0a8892fe31864718db64b
Dvdandrades <dvdandrades@gmail.com>
2026-03-09 10:28:14 +0100

    add settings

---
+++ stock_predictor/config/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    secret_key: str
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
+    env_name: str
+    database_url: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+@lru_cache
+def get_settings():
+    settings = Settings()
+    return settings
