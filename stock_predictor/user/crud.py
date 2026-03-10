5f412c648188ebbb9b0b77e3cbaa56c594b20cb7
Dvdandrades <dvdandrades@gmail.com>
2026-03-10 12:27:48 +0100

    add crud users

---
+++ stock_predictor/user/crud.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Session
+from stock_predictor.user import models, schemas
+
+
+def get_user_by_id(db: Session, user_id: int) -> models.User:
+    return (
+        db.query(models.User)
+        .filter(models.User.id == user_id, models.User.is_active == True)
+        .first()
+    )
+
+
+def get_user_by_username(db: Session, username: str) -> models.User:
+    return (
+        db.query(models.User)
+        .filter(models.User.username == username, models.User.is_active == True)
+        .first()
+    )
+
+
+def create_user(db: Session, user: schemas.UserCreate, hashed_password: str) -> models.User:
+    db_user = models.User(
+        username=user.username, email=user.email, hashed_password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def update_user(db: Session, db_user: models.User, updates: dict) -> models.User:
+    for key, value in updates.items():
+        setattr(db_user, key, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
